f3d55454d67fd656b19adc91168b102671768c5e
Don Moses <githinjidon@gmail.com>
2021-11-04 08:55:43 +0300

    add lists app to installed apps

--- task/settings.py
+++ task/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'list',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
